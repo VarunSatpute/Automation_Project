1ace074e31a0eaea9f032da9bba7d9142933da49
Varun Satpute <varunsatpute@gmail.com>
2022-12-03 22:20:50 +0530

    Task 3 commit

--- automation.sh
+++ automation.sh
@@ -44,3 +44,24 @@ if [[ -f /tmp/${name}-httpd-logs-${ts}.tar ]];
 then
     aws s3 cp /tmp/${name}-httpd-logs-${ts}.tar s3://${s3_bucket}/${name}-httpd-logs-${ts}.tar
 fi
+
+docroot="/var/www/html"
+
+#creating inventory.html if not created
+if [[ ! -f ${docroot}/inventory.html ]];
+then
+    echo -e 'Log Type&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Date Created&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Type&nbsp;&nbsp;&nbsp;Size' > ${docroot}/inventory.html
+fi
+
+#entering the logs to inventory.html
+if [[ -f ${docroot}/inventory.html ]];
+then
+    size=$(du -h /tmp/${name}-httpd-logs-${ts}.tar | awk '{print $1}')
+    echo -e "<br>httpd-logs&nbsp;&nbsp;&nbsp;&nbsp;${ts}&nbsp;&nbsp;&nbsp;&nbsp;tar&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${size}" >> ${docroot}/inventory.html
+fi
+
+#Creating cron job which runs at 12:00 am IST daily
+if [[ ! -f /etc/crom.d/automation ]];
+then
+    echo '30 18 * * * root /root/Automation_Project/automation.sh' >> /etc/cron.d/automation
+fi
